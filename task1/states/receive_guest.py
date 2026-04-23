78b3ac9db64c770989da0dbdc54aeb894193a38d
waiwai <123.qq.com>
2026-04-23 11:38:43 +0800

    修复range()关键词参数语法错误

--- task1/states/receive_guest.py
+++ task1/states/receive_guest.py
@@ -75,9 +75,7 @@ class ReceiveGuest(State):
             agv.navigate_to(agv.get_current_station(), config.STATION_START)
             wait_nav(timeout=config.NAV_TIMEOUT)
             #==== 空座位识别能力接口调用 START ==== author:xxy
-
-            max_attempts=5
-            for _ in range(max_attempts=5):
+            for _ in range(5):
                 color_frame, _ = self._cam_chest.get_frames()
                 if color_frame is None:
                     continue
